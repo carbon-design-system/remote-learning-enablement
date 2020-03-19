780eab0a48913265f662f72ef127d7ba6b603ada
Alison Joseph <alisonejoseph@gmail.com>
2020-03-19 13:55:01 -0500

    update site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,32 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Theme Carbon',
-    description: 'A Gatsby theme for the carbon design system',
-    keywords: 'gatsby,theme,carbon',
+    title: "Remote learning enablement",
+    description:
+      "A New Challenge for Teachers & Students. IBM has experience that can help",
+    keywords: "remote,learning,enablement, ibm"
   },
   pathPrefix: `/gtc`,
   plugins: [
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'Carbon Design Gatsby Theme',
-        short_name: 'Gatsby Theme Carbon',
-        start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#0062ff',
-        display: 'browser',
-      },
+        name: "Carbon Design Gatsby Theme",
+        short_name: "Gatsby Theme Carbon",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#0062ff",
+        display: "browser"
+      }
     },
     {
-      resolve: 'gatsby-theme-carbon',
+      resolve: "gatsby-theme-carbon",
       options: {
         repository: {
           baseUrl:
-            'https://github.com/carbon-design-system/gatsby-theme-carbon',
-          subDirectory: '/packages/example',
-        },
-      },
-    },
-  ],
+            "https://github.com/carbon-design-system/gatsby-theme-carbon",
+          subDirectory: "/packages/example"
+        }
+      }
+    }
+  ]
 };
