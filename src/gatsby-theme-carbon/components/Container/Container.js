c88bcc7693ad9cb348dec868d582cfb9946d4971
Scott <sstrubberg@us.ibm.com>
2020-03-19 14:29:14 -0500

    white theme

---
+++ src/gatsby-theme-carbon/components/Container/Container.js
@@ -0,0 +1,63 @@
+/* eslint-disable import/no-duplicates */
+import React, { useContext, useEffect, useRef, useCallback } from 'react';
+import classnames from 'classnames';
+import cx from 'classnames';
+import NavContext from 'gatsby-theme-carbon/src/util/context/NavContext';
+import useWindowSize from 'gatsby-theme-carbon/src/util/hooks/useWindowSize';
+import { overlay, visible } from './Container.module.scss';
+
+const Container = ({ children, homepage, theme }) => {
+  const { leftNavIsOpen, switcherIsOpen, toggleNavState } = useContext(
+    NavContext
+  );
+  const windowSize = useWindowSize();
+  const lastWindowSize = useRef(windowSize);
+
+  const closeNavs = useCallback(() => {
+    toggleNavState('leftNavIsOpen', 'close');
+    toggleNavState('switcherIsOpen', 'close');
+  }, [toggleNavState]);
+
+  useEffect(() => {
+    const windowShrinking =
+      lastWindowSize.current &&
+      lastWindowSize.current.innerWidth > windowSize.innerWidth;
+    if (windowShrinking && windowSize.innerWidth < 1056) {
+      closeNavs();
+    }
+    lastWindowSize.current = windowSize;
+  }, [closeNavs, windowSize, windowSize.innerWidth]);
+
+  const overlayVisible = (() => {
+    const navOpen = leftNavIsOpen || switcherIsOpen;
+    return navOpen && windowSize.innerWidth && windowSize.innerWidth < 1056;
+  })();
+
+  const containerClassNames = classnames({
+    container: theme !== 'dark' || !homepage,
+    'container--homepage': homepage,
+    'container--dark': theme === 'dark',
+    'container--white': theme === 'white',
+  });
+
+  return (
+    <>
+      <div
+        className={cx(overlay, { [visible]: overlayVisible })}
+        onClick={closeNavs}
+        onKeyPress={closeNavs}
+        role="presentation"
+        tabIndex="-1"
+      />
+      <main
+        id="main-content"
+        aria-hidden={overlayVisible}
+        className={containerClassNames}
+      >
+        {children}
+      </main>
+    </>
+  );
+};
+
+export default Container;
