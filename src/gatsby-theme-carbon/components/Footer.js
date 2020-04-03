8a930abb1d9a135e82ea1a1ec390b2092615e99c
Alison Joseph <alisonejoseph@gmail.com>
2020-04-03 14:00:22 -0500

    update footer

--- src/gatsby-theme-carbon/components/Footer.js
+++ src/gatsby-theme-carbon/components/Footer.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import Footer from 'gatsby-theme-carbon/src/components/Footer';
+import React from "react";
+import Footer from "gatsby-theme-carbon/src/components/Footer";
 
-const Content = () => (
+const currentYear = new Date().getFullYear();
+
+const Content = ({ buildTime }) => (
   <>
     <p>
-      The health of IBMers continues to be our first priority as the IBM Crisis
-      Management Teams and Corporate Health & Safety actively monitor the Novel
-      Coronavirus (COVID-19) disease outbreak.
-    </p>
-    <p>
-      We encourage you to stay healthy and keep informed of IBM recommendations
-      through w3 and through your local health authorities.
+      Last updated {buildTime}
+      <br />
+      Copyright © {currentYear} IBM
     </p>
   </>
 );
@@ -18,15 +16,24 @@ const Content = () => (
 const links = {
   firstCol: [
     {
-      href: 'https://w3.ibm.com/w3publisher/w3news/2020/novel-coronavirus',
-      linkText: 'W3 News',
+      href: "https://www.ibm.com/contact/us/en/?lnk=flg-cont-usen",
+      linkText: "Contact IBM"
     },
     {
-      href:
-        'https://w3.ibm.com/w3publisher/think-together/think-together-blog/7d7e8380-691e-11ea-8986-33ca10c8f5e0',
-      linkText: 'Message from Ginni',
-    },
+      href: "https://www.ibm.com/privacy/us/en/?lnk=flg-priv-usen",
+      linkText: "Privacy"
+    }
   ],
+  secondCol: [
+    {
+      href: "https://www.ibm.com/us-en/legal?lnk=flg-tous-usen",
+      linkText: "Terms of use"
+    },
+    {
+      href: "https://www.ibm.com/contact/us/en/?lnk=flg-cont-usen",
+      linkText: "Accessibility"
+    }
+  ]
 };
 
 const CustomFooter = () => <Footer links={links} Content={Content} />;
