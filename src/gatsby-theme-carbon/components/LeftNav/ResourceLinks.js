ca44bdc618126565cd2e5682729455ba5f1085dd
Alison Joseph <alisonejoseph@gmail.com>
2020-04-03 10:00:38 -0500

    remove links from left nav

--- src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
+++ src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
@@ -1,16 +1,7 @@
-import React from 'react';
-import ResourceLinks from 'gatsby-theme-carbon/src/components/LeftNav/ResourceLinks';
+import React from "react";
+import ResourceLinks from "gatsby-theme-carbon/src/components/LeftNav/ResourceLinks";
 
-const links = [
-  {
-    title: 'W3 News',
-    href: 'https://w3.ibm.com/w3publisher/w3news/2020/novel-coronavirus',
-  },
-  {
-    title: 'CDC Coronavirus information',
-    href: 'https://www.cdc.gov/coronavirus/2019-ncov/index.html',
-  },
-];
+const links = [];
 
 // shouldOpenNewTabs: true if outbound links should open in a new tab
 const CustomResources = () => <ResourceLinks shouldOpenNewTabs links={links} />;
