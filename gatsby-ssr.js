be595881c88c42cd295485d358b6af5fde97c813
andrea.cardonaibm.com <andrea.cardonaibm.com>
2020-05-26 16:21:52 -0500

    added tracking metrics sript & scss

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,35 @@
+/* eslint-disable react/no-danger */
 import React from 'react';
 
 export const onRenderBody = ({ setPostBodyComponents }) => {
+  const script = `
+  if(!window) window = {};
+  window.digitalData = {
+    page: {
+      category: {
+        primaryCategory: 'SB03'
+      },
+      pageInfo: {
+        language: 'en-US',
+        publisher: 'IBM Corporation',
+        ibm: {
+          country: 'US',
+          owner: 'Kristin Wisniewski',
+          developer: 'The Cleaner',
+          siteID: 'GLOBAL',
+          subject: 'IBM000',
+        }
+      }
+    };
+`;
 
   return setPostBodyComponents([
+    <script key="digital-data" dangerouslySetInnerHTML={{ __html: script }} />,
     <script
-      key="ibm-common"
-      src="//1.www.s81c.com/common/stats/ibm-common.js"
+      defer
+      key="core-metrics"
+      src="https://1.www.s81c.com/common/stats/ida_stats.js"
+      type="text/javascript"
     />,
-  ])
+  ]);
 };
